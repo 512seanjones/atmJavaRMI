69b4215d15b70aea8f14ea1b24a29004089b3f21
Sean Jones <sjones7@stevens.edu>
2020-04-19 10:58:47 -0400

    Added client menu interface and added additional Atm function to check if account exists

--- Atm.java
+++ Atm.java
@@ -1,8 +1,10 @@
 import java.rmi.*;
 
 public interface Atm extends java.rmi.Remote {
-    // Print success or failure
-    String accessAccount(String accountNum, String accountPin) throws RemoteException;
+    // Returns true or false
+    boolean lookupAccount(String accountNum) throws RemoteException;
+    // Returns true or false
+    boolean accessAccount(String accountNum, String accountPin) throws RemoteException;
     // Print success with balance or failure
     String getBalance() throws RemoteException;
     // Print success with new balance or failure
--- AtmClient.java
+++ AtmClient.java
@@ -1,11 +1,70 @@
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 public class AtmClient {
     public static void main(String arg[]){
         try {
-            String ipAddress = "10.0.0.201";
+            String ipAddress = "192.168.0.119";
             Atm obj = (Atm) Naming.lookup("//" + ipAddress + "/AtmServer");
+
+            Scanner in = new Scanner(System.in);
+            while (true) {
+                System.out.println("Please enter an account number:");
+                String accountNum = in.nextLine();
+                if (!obj.lookupAccount(accountNum)) {
+                    System.out.println("Sorry that account does not exist");
+                    continue;
+                }
+                System.out.println("Please enter a pin number:")
+                String pin = in.nextLine();
+                if (obj.accessAccount(accountNum, pin)) {
+                    boolean inAccount = true;
+                    while (inAccount) {
+                        System.out.println("\n");
+                        System.out.println("Current account balance is: " + obj.getBalance());
+                        System.out.println("\n");
+                        System.out.println("Please select an atm option:");
+                        System.out.println("\t1. Withdraw from account");
+                        System.out.println("\t2. Deposit to account");
+                        System.out.println("\t3. Exit account");
+                        choice = in.nextLine();
+                        switch (choice) {
+                            case "1": {
+                                System.out.println("Enter deposit amount:")
+                                float amount = in.nextFloat();
+                                System.out.println(obj.withdraw(amount));
+                            }
+                            case "2": {
+                                System.out.println("Enter withdraw amount")
+                                float amount = in.nextFloat();
+                                System.out.println(obj.deposit(amount));
+                                break;
+                            }
+                            case "3": {
+                                System.out.println(obj.exitAccount());
+                                // Stop looping
+                                inAccount = false;
+                                break;
+                            }
+                            default: {
+                                System.out.println("Please select an option from the menu");
+                            }
+                        }
+                    }
+                }
+                else {
+                    System.out.println("Invalid pin number");
+                    continue;
+                }
+                System.out.println("Would you like to access another account (y or n)?");
+                exitChoice = in.nextLine();
+                if (exitChoice != "y") {
+                    System.out.println("Goodbye");
+                    break;
+                }
+            }
+
             System.out.println(obj.accessAccount("0002", "5555"));
             System.out.println(obj.deposit(100.00));
             System.out.println(obj.withdraw(25));
